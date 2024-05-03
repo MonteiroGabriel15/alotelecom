d326f7bd19c0dde312af983f3830f0a8d2c1c9a6
Gabriel Monteiro <gfonseca3117@gmail.com>
2024-05-03 15:11:24 -0300

    estilização do header

--- alotelecom/src/components/micros/header/index.jsx
+++ alotelecom/src/components/micros/header/index.jsx
@@ -1,9 +1,12 @@
 import React from "react";
-import { HeaderComponent, HomeButtonComponent, IntitucionalButtonComponent, AboutUsButtonComponent, ContactButtonComponent,ButtonsContainer } from "./style";
+import { HeaderComponent, HomeButtonComponent, IntitucionalButtonComponent, AboutUsButtonComponent, ContactButtonComponent,ButtonsContainer, LogoContainer, LogoImage } from "./style";
+import imagem1 from '../../../assets/imagem1.png'
 const Header = () => {
     return(
         <HeaderComponent>
-            AloTelecom
+            <LogoContainer>
+                <LogoImage src = {imagem1} alt="logo" />
+            </LogoContainer>
             <ButtonsContainer>
                 <HomeButtonComponent>
                     Home
--- alotelecom/src/components/micros/header/style.jsx
+++ alotelecom/src/components/micros/header/style.jsx
@@ -4,31 +4,64 @@ import styled from "styled-components";
 export const HeaderComponent = styled.header`
 display: flex;
 flex-direction: row;
-justify-content: space-between;
+justify-content: space-evenly;
 align-items:center;
 width: 100%;
 height: 6rem;
-background-color: red;
+background-color: #FFFFFFFF;
 `
 
 
 export const HomeButtonComponent = styled.button`
-width: 5rem;
-height: 5vh;
+width: 7rem;
+height: 10.4vh;
+background-color: #FFFFFFFF;
+color: #7D0100;
+border: none;
+border-bottom: #7D0100 solid;
+&:hover{
+    border-bottom: #7D0100 5px solid;
+}
+font-size: large
 `
+
 export const IntitucionalButtonComponent = styled.button`
-width: 5rem;
-height: 5vh;
+width: 7rem;
+height: 10.4vh;
+background-color: #FFFFFF;
+color: #7D0100;
+border: none;
+border-bottom: #7D0100 solid;
+&:hover{
+    border-bottom: #7D0100 5px solid;
+}
+font-size: large
 `
 
 export const AboutUsButtonComponent = styled.button`
-width: 5rem;
-height: 5vh;
+width: 7rem;
+height: 10.4vh;;
+background-color: #FFFFFFFF;
+color: #7D0100;
+border: none;
+border-bottom: #7D0100 solid;
+&:hover{
+    border-bottom: #7D0100 5px solid;
+}
+font-size: large
 `
 
 export const ContactButtonComponent = styled.button`
-width: 5rem;
-height: 5vh;
+width: 7rem;
+height: 10.4vh;
+background-color: #FFFFFFFF;
+color: #7D0100;
+border: none;
+border-bottom: #7D0100 solid;
+&:hover{
+    border-bottom: #7D0100 5px solid;
+}
+font-size: large
 `
 
 export const ButtonsContainer = styled.div`
@@ -36,7 +69,20 @@ display: flex;
 flex-direction: row;
 align-items: center;
 justify-content: space-evenly;
-width: 30rem;
+width: 40rem;
 height: 100%;
-background-color: blue;
+background-color: #FFFFFF;
+padding: 2rem 1em;
 `
+
+export const LogoContainer = styled.div`
+width: 12.5rem;
+height:6rem;
+`
+
+export const LogoImage = styled.img`
+width: 10rem;
+height:6rem;
+`
+
+
