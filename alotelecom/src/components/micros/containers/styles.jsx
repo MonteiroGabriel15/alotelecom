67bfd6803df41ea98ead976559efee12cfe210e1
Gabriel Monteiro <gfonseca3117@gmail.com>
2024-05-10 14:12:54 -0300

    consertando a imagem no background

--- alotelecom/src/components/micros/containers/styles.jsx
+++ alotelecom/src/components/micros/containers/styles.jsx
@@ -2,9 +2,10 @@ import styled from "styled-components"
 import coinorginal from "../../../assets/coindimensionada.jpg"
 
 export const ImgContainer = styled.div`
-width: 2542.4px;
-height: 140.5vh;
+width:  99.3125vw;
+height: 90.5vh;
 background-image: url(${coinorginal});
-top: -800px;
+`
+export const TextContainer = styled.div`
 `
 
