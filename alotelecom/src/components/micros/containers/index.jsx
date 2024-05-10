bc12ea01ff76f290ad5bf5fa3524feda9ba7e7ae
Gabriel Monteiro <gfonseca3117@gmail.com>
2024-05-10 17:02:02 -0300

    introdução do componente de mensagem de boas vindas -TextContainer

--- alotelecom/src/components/micros/containers/index.jsx
+++ alotelecom/src/components/micros/containers/index.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { ImgContainer,TextImageContainer } from "./styles";
+import { ImgContainer,TextContainer } from "./styles";
 export const WelcomeContainer = () => {
     return(
         <ImgContainer>
+            <TextContainer><h1>Seja bem Vindo!</h1> <p>adipisicing elit. Magni earum nemo, odit pariatur temporibus eos, vero, tenetur delectus ut hic ullam dolorem numquam maiores blanditiis expedita re</p> <p>prehenderit corporis unde praesentium</p></TextContainer>
         </ImgContainer>
     )
 }
--- alotelecom/src/components/micros/containers/styles.jsx
+++ alotelecom/src/components/micros/containers/styles.jsx
@@ -2,10 +2,28 @@ import styled from "styled-components"
 import coinorginal from "../../../assets/coindimensionada.jpg"
 
 export const ImgContainer = styled.div`
-width:  99.3125vw;
+display: flex;
+width:  100%;
 height: 90.5vh;
 background-image: url(${coinorginal});
 `
 export const TextContainer = styled.div`
+display: flex;
+position: relative;
+top: 15%;
+left: 1%;
+flex-direction: column;
+justify-content: space-evenly;
+width: 25%;
+height: 25%;
+color: #FFFFFF;
+h1{
+    color:#cc3d3d;
+    font-weight: bold;
+}
+
+p{
+    color: #6c757d;
+}
 `
 
